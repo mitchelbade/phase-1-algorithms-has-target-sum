ad025744bbcffe9c7d43615966b7080f83d176d3
mitchelbade <mitchelm.bade@gmail.com>
2022-02-05 19:00:18 -0800

    commit tree 34cd380b5aff0661b669fa581bb813826a80a48b

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // for (let i = 0; i < array.length; i++) {
+  //   for (let j = i + 1; j < array.length; j++) {
+  //     if (array[i] + array[j] === target) {
+  //       return true;
+  //     }
+  //   }
+  // }
+  // return false;
+  // ---------------------------------------------------------
+  const numI = new Map();
+
+  for (let i = 0; i < array.length; i++) {
+    const num = array[i];
+    const comp = target - num;
+
+    if (numI.has(comp)) return true;
+    numI.set(num, i);
+    // console.log(numI);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+    first solution - Time: O(n^2) Space: O(n)
+
+    second solution - Time:O(n) Space:O(n)
 */
 
 /* 
   Add your pseudocode here
+  create a function that takes in an array and a target number
+    iterate through the array
+      if any pair of numbers is equal to the target number
+        return true
+      else if
+        return false
+        ----------------------------------
+  create a function that takes in an array and a target number
+    create Map() object
+
+    iterate through the array
+      create a variable for current number of array
+      create a variable for the compliment number
+
+      if Map() has compliment
+        return true
+        add number and index to Map()
+
+    if no compliment is found return false
 */
 
 /*
   Add written explanation of your solution here
+
+  create a function that takes in an array and a target number,
+  if a pair of numbers from the array are equal to the target number return true,
+  if not return false.
 */
 
 // You can run `node index.js` to view these console logs
